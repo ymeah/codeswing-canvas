25a09f94ff07bfaafd4b70e8a5df00cadf30750e
ymeah <112223055+ymeah@users.noreply.github.com>
2023-03-05 11:24:20 -0600

    I changed the color

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ c.strokeRect(0, 0, 500, 400)
 c.fillStyle = 'blue'
 c.fillRect(10, 10, 480, 380)
 
-c.fillStyle = 'orange'
+c.fillStyle = 'yellow'
 c.font = 'small-caps bold 24px sans-serif'
 c.textAlign = 'center'
-c.fillText('Hello code hackerz!', 250, 200)
+c.fillText(' Hello master coders !', 250, 200)
